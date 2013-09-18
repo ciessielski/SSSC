5f7d912cf814283ab5601b708c24ebb248ca51c2
Michal Ciesielski <ciessielski@gmail.com>
2013-09-18 18:00:16 -0400

    1

---
+++ marynarzykCounterTests/marynarzykCounterTests.h
@@ -0,0 +1,13 @@
+//
+//  marynarzykCounterTests.h
+//  marynarzykCounterTests
+//
+//  Created by Mielczarek on 8/14/13.
+//  Copyright (c) 2013 Mielczarek. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface marynarzykCounterTests : SenTestCase
+
+@end
---
+++ marynarzykCounterTests/marynarzykCounterTests.m
@@ -0,0 +1,32 @@
+//
+//  marynarzykCounterTests.m
+//  marynarzykCounterTests
+//
+//  Created by Mielczarek on 8/14/13.
+//  Copyright (c) 2013 Mielczarek. All rights reserved.
+//
+
+#import "marynarzykCounterTests.h"
+
+@implementation marynarzykCounterTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in marynarzykCounterTests");
+}
+
+@end
